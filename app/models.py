930a8e0a2f341b3bb1c15ace2f5499a9e922f614
taurus5650 <taurus_5650@hotmail.com>
2026-03-19 08:28:05 +0800

    feat: define Item data model

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
